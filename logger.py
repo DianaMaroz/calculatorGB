8588db285013a636b36fcd375b305595acd132dd
Diana\ Moroz <diva14@yandex.by>
2022-12-13 12:06:14 +0300

    what Diana's done: string calculate + logger

--- logger.py
+++ logger.py
@@ -4,3 +4,8 @@ def logger(first, second, oper, res):
     log = f'{dt.now()} | {first} {oper} {second} = {res}\n'
     with open(path, 'a') as data:
         data.write(log)
+
+def string_logger(expression, res):
+    log = f'{dt.now()} | {expression} = {res}\n'
+    with open(path, 'a') as data:
+        data.write(log)
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 first_number = 0
 intermediate_result = 0
 next_number = 0
+calc_string = ''
 
 def summa() -> int:
     global intermediate_result
@@ -67,3 +68,49 @@ def get_first_number():
 def set_first_number(smth):
     global first_number
     first_number = smth
+
+def get_calc_string():
+    global calc_string
+    return calc_string
+
+def set_calc_string(smth):
+    global calc_string
+    calc_string = smth
+
+def calculate_str(sm_str) -> int:
+    sm_list = sm_str.split()
+    for i in range(len(sm_list)):
+        if sm_list[i].isdigit():
+            sm_list[i] = int(sm_list[i])
+    while ('*' in sm_list) or ('/' in sm_list):
+        i = 0
+        while i < len(sm_list):
+            if sm_list[i] == "*":
+                sm_list[i-1] = sm_list[i-1] * sm_list[i+1]
+                sm_list.remove(sm_list[i])
+                sm_list.remove(sm_list[i])
+
+            elif sm_list[i] == "/":
+                sm_list[i-1] = sm_list[i-1] / sm_list[i+1]
+                sm_list.remove(sm_list[i])
+                sm_list.remove(sm_list[i])
+
+            else:
+                i += 1
+
+    while ('+' in sm_list) or ('-' in sm_list):
+        i = 0
+        while i < len(sm_list):
+            if sm_list[i] == "+":
+                sm_list[i-1] = sm_list[i-1] + sm_list[i+1]
+                sm_list.remove(sm_list[i])
+                sm_list.remove(sm_list[i])
+
+            elif sm_list[i] == "-":
+                sm_list[i-1] = sm_list[i-1] - sm_list[i+1]
+                sm_list.remove(sm_list[i])
+                sm_list.remove(sm_list[i])
+            else:
+                i += 1
+
+    return sm_list[0]
